9d546bcf98f1364e7e3083a015751e4fec6faea3
Maffergu <60632469+Maffergu@users.noreply.github.com>
2020-09-07 22:27:42 -0600

    Update diaria.java

--- diaria.java
+++ diaria.java
@@ -1,6 +1,15 @@
 //tomar en cuenta que la clase al momento de tener el ejercicio bueno debe retornar un 1
 public class diaria{
-public diaria(){
-
-}
+  String retornod;
+  public String diari(){
+    retornod = "Actualmente se encuentra en mantenimiento la sección de ejercicios de vida cotidiana";
+    return retornod ;
+  }
+  /*public int calificad(int op){
+    if (op == 1){
+      return 1;
+    }else if(op ==2){
+      return 0;
+    }
+  }*/
 }
