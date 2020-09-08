b6d7ca822622eacb26dc9fbb05003d245f6f5ce7
Maffergu <60632469+Maffergu@users.noreply.github.com>
2020-09-07 22:25:01 -0600

    Update familia.java

--- familia.java
+++ familia.java
@@ -1,6 +1,15 @@
 //tomar en cuenta que la clase al momento de tener el ejercicio bueno debe retornar un 1
 public class familia{
-public familia(){
-
-}
+  String retornof;
+  public String famili(){
+    retornof = "Actualmente se encuentra en mantenimiento la sección de ejercicios de familia";
+    return retornof ;
+  }
+  /*public int calificaf(int op){
+    if (op == 1){
+      return 1;
+    }else if(op ==2){
+      return 0;
+    }
+  }*/
 }
