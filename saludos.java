973253459866983f871b9cf2fc06d9efd4300543
Maffergu <60632469+Maffergu@users.noreply.github.com>
2020-09-07 22:25:33 -0600

    Update saludos.java

--- saludos.java
+++ saludos.java
@@ -1,8 +1,15 @@
 //tomar en cuenta que la clase al momento de tener el ejercicio bueno debe retornar un 1
 public class saludos{
-
-
-public saludos(){
-
-}
+  String retornos;
+  public String saluda(){
+    retornos = "Actualmente se encuentra en mantenimiento la sección de ejercicios de saludos";
+    return retornos ;
+  }
+  /*public int calificas(int op){
+    if (op == 1){
+      return 1;
+    }else if(op ==2){
+      return 0;
+    }
+  }*/
 }
